54b893be41e3dc2acfb9c88d97713e10ed1f325a
Erik Augusto Lima Evangelista <eriklim@hotmail.com>
2021-10-13 16:15:47 -0300

    segundo-commit-curso-cypress-13-10-2021

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -70,18 +70,38 @@ describe('Work with basic elements', () => {
         cy.get('#formComidaVegetariana').should('be.checked')
     })
 
-    it('ComboBox', () => {
+    it('Combo', () => {
+        cy.get('[data-test=dataEscolaridade]')
+            .select('2o grau completo')
+            .should('have.value', '2graucomp')
+
         cy.get('[data-test=dataEscolaridade]')
             .select('1graucomp')
             .should('have.value', '1graucomp')
-
-        // Validar as opções do combo    
+            
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function() {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })           
     })
 
     it.only('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
-            .select(['natacao', 'Corrida'])
+            .select(['natacao', 'Corrida', 'nada'])
+        // cy.get('[data-testid=dataEsportes]').should('have.value', ['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
 
-        // TODO validar opções selecionadas do combo múltiplo    
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
+
     })
 })
